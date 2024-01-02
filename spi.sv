dd3e68a742597b29e76e70548a1cf1f43f31d716
crispy245 <aldobalsamoreyes@gmail.com>
2024-01-02 14:54:45 +0800

    Modify testbench to check tx and rx

--- spi.sv
+++ spi.sv
@@ -3,14 +3,13 @@ module spi (
     input wire i_rst,
 
     input [7:0] i_tx_byte,
-    input wire tx_start,
+    input wire i_tx_start,
+    output reg o_tx_ready,
 
     output [7:0] o_rx_byte,
-
     
-    input reg i_poci,
-
     output wire o_sclk,
+    input reg i_poci,
     output reg o_pico,
     output reg o_cs
 );  
@@ -20,8 +19,8 @@ module spi (
     localparam TRANS = 1'b1;
     reg state;
     reg next_state;
-    reg [7:0] data_in;
-    reg [7:0] data_out;
+    reg [7:0] shift_out;
+    reg [7:0] shift_in;
     reg [2:0] bit_count;
 
     always @(posedge i_clk)
@@ -31,36 +30,42 @@ module spi (
     always  @(posedge i_clk) begin
         case(state)
             IDLE: begin
-                if(tx_start) begin
+                if(i_tx_start) begin
                     o_cs = 0;
-                    data_in = i_tx_byte;
-                    o_pico = data_in[7];
+                    shift_out = i_tx_byte;
+                    o_rx_byte = {o_rx_byte[6:0],i_poci};
+                    o_pico = shift_out[7];
                     next_state = TRANS;
                 end
                 else begin
                     o_pico = 0;
                     o_cs = 1;
-                    data_in = 0;
+                    shift_out = 0;
                     next_state = IDLE;
                 end
+                o_tx_ready = 0;
             end
             TRANS: begin
-                data_in = {data_in[6:0], 1'b0};
+                shift_out = {shift_out[6:0], 1'b0};
                 o_sclk = ~o_sclk;
                 if(bit_count == 3'b111) begin
-                    if(tx_start) begin
+                    if(i_tx_start) begin
                         o_cs = 0;
-                        data_in = i_tx_byte;
+                        shift_out = i_tx_byte;
                         next_state = TRANS;
                     end
                     else begin
                         o_cs = 1;
                         next_state = IDLE;
                     end
+                    o_rx_byte = 0;
+                    o_tx_ready = 1;
                     bit_count = 0;
                 end
                 else begin
-                    o_pico = data_in[7];
+                    o_tx_ready = 0;
+                    o_pico = shift_out[7];
+                    o_rx_byte = {o_rx_byte[6:0],i_poci};
                     bit_count = bit_count + 1;
                 end
             end
--- tb_spi.cpp
+++ tb_spi.cpp
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <iostream>
+#include <vector>
 #include <verilated.h>
 #include <verilated_vcd_c.h>
 #include "Vspi.h"
@@ -9,6 +10,16 @@ vluint64_t sim_time = 0;
 Vspi *dut = new Vspi;
 VerilatedVcdC *m_trace = new VerilatedVcdC;
 
+std::vector<uint8_t> to_binary(uint8_t num) {
+    std::vector<uint8_t> binaryVector;
+
+    for (int i = 7; i >= 0; --i) {
+        binaryVector.push_back((num & (1 << i)) ? 1 : 0);
+    }
+
+    return binaryVector;
+}
+
 void clk(){
     dut->eval();
     dut->i_clk ^=1;
@@ -17,10 +28,41 @@ void clk(){
     sim_time++;
 }
 
-void send_byte(){
-    dut->tx_start = 1;
-    for(int  i= 0; i < 16; i++) clk();
-    dut->tx_start = 0;
+void send_byte(uint8_t input_byte){
+    dut->i_tx_byte = input_byte;
+    dut->i_tx_start = 1;
+    for(int  i= 0; i < 8; i++) {
+        clk();
+        clk();
+    }
+    dut->i_tx_start = 0;
+    dut->i_tx_byte = 0;
+}
+
+void recieve_byte(u_int8_t input_byte){
+    std::vector<uint8_t> input_byte_binary = to_binary(input_byte);
+    dut->i_tx_start = 1;
+    for(int  i= 0; i < 8; i++){ 
+        printf("%d",(int)input_byte_binary[i]);
+        dut->i_poci = input_byte_binary[i];
+        clk();
+        clk();
+    }
+    dut->i_poci = 0;
+    dut->i_tx_start = 0; 
+}
+
+void send_recieve_byte(u_int8_t input_byte_tx, u_int8_t input_byte_rx){
+    std::vector<uint8_t> input_byte_rx_binary = to_binary(input_byte_rx);
+    dut->i_tx_start = 1;
+    dut->i_tx_byte = input_byte_tx;
+    for(int i = 0; i < 8; i++){
+        dut->i_poci = input_byte_rx_binary[i];
+        clk();
+        clk();
+    }
+    dut->i_poci = 0;
+    dut->i_tx_start = 0;
 }
 int main(int argc, char** argv, char** env) {
 
@@ -28,14 +70,12 @@ int main(int argc, char** argv, char** env) {
     dut->trace(m_trace, 5);
     m_trace->open("waveform.vcd");
 
-        dut->i_tx_byte = 7;
-        send_byte();
-        dut->i_tx_byte = 255;
-        send_byte();
+        send_byte(7);
+        send_byte(255);
+        recieve_byte(15);
+        send_recieve_byte(4,4);
         clk();
         clk();
-
-
 
     m_trace->close();
     delete dut;
